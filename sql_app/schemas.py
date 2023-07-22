2f1620bed4377bc5524481a6a3d14c232e75c592
nazmul-cse48 <nazmul.cse48@gmail.com>
2023-07-22 16:47:00 +0600

    schemas

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,17 +1,46 @@
+from typing import Optional
 from pydantic import BaseModel
+import datetime
+from fastapi import Body
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
+class User(BaseModel):
+    username: str
 
-class ItemCreate(ItemBase):
-    pass
+class UserInDB(User):
+    hashed_password: str
 
+class UserCreate(User):
+    password: str
+    email: str
 
-class Item(ItemBase):
-    id: int
+class PostBase(BaseModel):
+    title:str
+    body:str
+
+class PostList(PostBase):
+    created_date: Optional[datetime.datetime]
+    owner_id: int
+    owner: User
+
     class Config:
-        orm_mode = True
+        orm_mode=True
 
+class CommentBase(BaseModel):
+    name:str
+    body:str
+    email:str
+
+class CommentList(CommentBase):
+    id: int
+    post_id:int
+    created_date: Optional[datetime.datetime]= Body(None)
+
+    class Config:
+        orm_mode= True
